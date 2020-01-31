3e78327a0955c85b90aeb7b0223ab71bd54d9456
Bilal Amin khan <pukhtoon004@gmail.com>
2020-01-31 18:18:10 +0500

    Updates

---
+++ precache-manifest.5039c927c8d760dde4f6d922d8bce470.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68de6a987e5f136acf488a7c782b7112",
+    "url": "/thegetfit/index.html"
+  },
+  {
+    "revision": "ce70ea65855633878eaf",
+    "url": "/thegetfit/static/css/2.66a1a0a7.chunk.css"
+  },
+  {
+    "revision": "7dc56664078e8b69c99b",
+    "url": "/thegetfit/static/css/main.d6c68e6d.chunk.css"
+  },
+  {
+    "revision": "ce70ea65855633878eaf",
+    "url": "/thegetfit/static/js/2.369f0dac.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/thegetfit/static/js/2.369f0dac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7dc56664078e8b69c99b",
+    "url": "/thegetfit/static/js/main.1e9fafbd.chunk.js"
+  },
+  {
+    "revision": "e3a5a6c2e21565e83a9f",
+    "url": "/thegetfit/static/js/runtime-main.3733ac24.js"
+  },
+  {
+    "revision": "1c2fa66f2821c5c418c14f7e804dd49a",
+    "url": "/thegetfit/static/media/1.1c2fa66f.svg"
+  },
+  {
+    "revision": "b798c2219637b58d4056f3f748c5e8e5",
+    "url": "/thegetfit/static/media/1.b798c221.svg"
+  },
+  {
+    "revision": "52c9bd0e77a25cdc521c1a916a27cf64",
+    "url": "/thegetfit/static/media/2.52c9bd0e.svg"
+  },
+  {
+    "revision": "fd4a4b31cdc361834d29420cbf71357e",
+    "url": "/thegetfit/static/media/2.fd4a4b31.svg"
+  },
+  {
+    "revision": "0f684ac36f9af392d922a075dd9e5bb4",
+    "url": "/thegetfit/static/media/3.0f684ac3.svg"
+  },
+  {
+    "revision": "50f319095f8c15772339a2c5439a1218",
+    "url": "/thegetfit/static/media/3.50f31909.svg"
+  },
+  {
+    "revision": "3008383b8def33b4dc186133be8269a9",
+    "url": "/thegetfit/static/media/4.3008383b.svg"
+  },
+  {
+    "revision": "4f464ad589a043db5083af1616c9e8b9",
+    "url": "/thegetfit/static/media/4.4f464ad5.svg"
+  },
+  {
+    "revision": "2813587d3425de8ebb4375b2a8f452f6",
+    "url": "/thegetfit/static/media/BMI.2813587d.svg"
+  },
+  {
+    "revision": "f920a2e732fb2e102c60d735f923e90d",
+    "url": "/thegetfit/static/media/about.f920a2e7.svg"
+  },
+  {
+    "revision": "64c4c303bc24256ba5e081a106415411",
+    "url": "/thegetfit/static/media/contact.64c4c303.svg"
+  },
+  {
+    "revision": "3008383b8def33b4dc186133be8269a9",
+    "url": "/thegetfit/static/media/diet.3008383b.svg"
+  },
+  {
+    "revision": "de122b33cd2e103402f8fb425f32f67e",
+    "url": "/thegetfit/static/media/email.de122b33.svg"
+  },
+  {
+    "revision": "3760bc9649a02dc34c1736491a55ed00",
+    "url": "/thegetfit/static/media/exercise.3760bc96.svg"
+  },
+  {
+    "revision": "ace0e6725a22c2e73df455bbc670ea0e",
+    "url": "/thegetfit/static/media/exercise2.ace0e672.svg"
+  },
+  {
+    "revision": "2be4a49fa2f9523747e44a0461b2aa8a",
+    "url": "/thegetfit/static/media/facebook.2be4a49f.svg"
+  },
+  {
+    "revision": "16d5a72b68df6ac8a78b505a70e62ffb",
+    "url": "/thegetfit/static/media/instagram.16d5a72b.svg"
+  },
+  {
+    "revision": "0989fedbe54ef6bab6d906610308438b",
+    "url": "/thegetfit/static/media/landing.0989fedb.svg"
+  },
+  {
+    "revision": "cd2155261c30465d882e6b74bcf2e10d",
+    "url": "/thegetfit/static/media/logo_.cd215526.svg"
+  },
+  {
+    "revision": "79b47f0fb01ac8389f3417e206632289",
+    "url": "/thegetfit/static/media/mob.79b47f0f.svg"
+  },
+  {
+    "revision": "a2613497be6c4d39fde45cec133ea7f0",
+    "url": "/thegetfit/static/media/privateClass.a2613497.svg"
+  },
+  {
+    "revision": "9cc41d42ca6207379b1187e22981857a",
+    "url": "/thegetfit/static/media/scale.9cc41d42.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/thegetfit/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/thegetfit/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/thegetfit/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/thegetfit/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5f5c748f558b94c8d7475e70946c61db",
+    "url": "/thegetfit/static/media/spinning.5f5c748f.svg"
+  },
+  {
+    "revision": "74ffda38148c99132d827d0452998907",
+    "url": "/thegetfit/static/media/testimonials.74ffda38.svg"
+  },
+  {
+    "revision": "7eb480bf209607d3ca43eca154a4581b",
+    "url": "/thegetfit/static/media/twitter.7eb480bf.svg"
+  }
+]);
